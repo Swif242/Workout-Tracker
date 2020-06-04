2467dc0f0ce312856f1519f3b58dd2799f07e0a2
Jeremy Nelson <swif242@hotmail.com>
2020-06-03 19:21:00 -0700

    fixed routes, had to replace db with the const workout. also added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,61 +3,61 @@ const workout = require("../models/workout.js");
 
 // get all workouts
 router.get("/api/workouts", (req, res) => {
-   workout.find({})
-      .then(dbworkout => {
-        res.json(dbworkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  router.post("/api/workouts", async (req, res) => {
-    try {
-        const response = await workout.create({ type: "workout" })
-        res.json(response);
-    }
-    catch (err) {
-        console.log("Error occurred creating a workout: ", err)
-    }
+  workout.find({})
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+// post new workout 
+router.post("/api/workouts", async (req, res) => {
+  try {
+    const response = await workout.create({ type: "workout" })
+    res.json(response);
+  }
+  catch (err) {
+    console.log("Error occurred creating a workout: ", err)
+  }
 });
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-    const workoutId = params.id;
-    let savedExercises = [];
+  const workoutId = params.id;
+  let savedExercises = [];
 
-    workout.find({ _id: workoutId })
-        .then(dbWorkout => {
-            savedExercises = dbWorkout[0].exercises;
-            res.json(dbWorkout[0].exercises);
-            let allExercises = [...savedExercises, body]
-            console.log(allExercises)
-            updateWorkout(allExercises)
-        })
-        .catch(err => {
-            res.json(err);
-        });
+  workout.find({ _id: workoutId })
+    .then(dbWorkout => {
+      savedExercises = dbWorkout[0].exercises;
+      res.json(dbWorkout[0].exercises);
+      let allExercises = [...savedExercises, body]
+      console.log(allExercises)
+      updateWorkout(allExercises)
+    })
+    .catch(err => {
+      res.json(err);
+    });
 
-
-    function updateWorkout(exercises) {
-      workout.findByIdAndUpdate(workoutId, { exercises: exercises }, function (err, doc) {
-            if (err) {
-                console.log(err)
-            }
-        });
-    };
+// update workout
+  function updateWorkout(exercises) {
+    workout.findByIdAndUpdate(workoutId, { exercises: exercises }, function (err, doc) {
+      if (err) {
+        console.log(err)
+      }
+    });
+  };
 });
 
 
 router.get("/api/workouts/range", (req, res) => {
   workout.find({})
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 
-  module.exports = router;
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,15 @@
 const router = require("express").Router();
 const path = require("path");
 
-
+// goes to the home page when first visited
   router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-
+// goes to the add exercise page
   router.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-
+// goes to the dashboard page 
   router.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
